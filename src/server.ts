85595ce360349845296eaddd790e2c83c12b35ab
Allan Lobato <allandonizetti@gmail.com>
2023-11-28 18:55:26 -0300

    Estrutura do projeto - Configurando ESLint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import { env } from "./env"
-import {app} from "./app"
+import { env } from './env'
+import { app } from './app'
 
-app.listen({
-    host: '0.0.0.0', //hack para que a aplicação seja mais acessível para o frontend
+app
+  .listen({
+    host: '0.0.0.0', // hack para que a aplicação seja mais acessível para o frontend
     port: env.PORT,
-}).then(() => {
+  })
+  .then(() => {
     console.log('HTTP Server running!')
-})
+  })
