4301f1a8542674b740fc994e5977bc06f3c81484
Allan Lobato <allandonizetti@gmail.com>
2023-11-29 14:51:50 -0300

    Integração com Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Alice',
+    email: 'alice@rocketseat.com.br',
+  },
+})
+
+// fastify é um framework para criação de aplicações web, que é baseado em plugins e decorators.
